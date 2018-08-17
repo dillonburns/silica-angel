8af516171c270bdcccc40c5d888abafe6ed42403
Dillon Burns <reach.dillon@gmail.com>
2018-08-17 18:59:56 -0400

    draft 3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,22 +2,21 @@
   <div class="content">
     <img src="@/assets/silica_angel_logo.png">
     <div class="message">
-      little winds blowing backwards into u over u out of u
+      <span>little winds blowing backwards into u over u out of u</span>
     </div>
-    <div class="click">
-      thankful
+    <div class="click"
+         @click="togglePlaying">
+      <span>thankful</span>
     </div>
     <div class="contact">
       <a href="mailto:contact@silicaangel.rip">
-        contact@silicaangel.rip
+        <span>contact@silicaangel.rip</span>
       </a>
     </div>
-    <!--
     <audio ref="audio"
            loop
            :src="snippetSrc"
            class="audio"/>
-    -->
   </div>
 </template>
 
@@ -44,6 +43,7 @@ export default {
         this.$refs.audio.play()
       } else {
         this.$refs.audio.pause()
+        this.$refs.audio.currentTime = 0
       }
     }
   }
@@ -67,21 +67,34 @@ $fake-italic: skew(-10deg, 0);
     margin-bottom: 36px;
   }
 
+  .message,
+  .click,
+  .contact {
+    transform: $fake-italic;
+
+    &:not(.message) {
+      margin: 16px auto;
+
+      &:hover {
+        span{
+          width: auto;
+          cursor: pointer;
+          text-decoration: underline dotted;
+        }
+      }
+    }
+  }
+
   .message {
     font-size: 2.75vmax;
-    transform: $fake-italic;
   }
 
   .click {
     font-size: 3.2vmax;
-    margin: 16px auto;
-    transform: $fake-italic;
   }
 
   .contact {
     font-size: 2vmax;
-    margin: 16px auto;
-    transform: $fake-italic;
 
     a {
       color: inherit;
